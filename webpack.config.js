6770769f174a1c1856e21f2efe880c8e3478380b
Tryd0g0lik <work80@mail.ru>
2023-12-13 10:32:04 +0700

    [master]: The fierst commit

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	mode: 'none',
+	entry: './src/index.js',
+	target: 'web',
+	devServer: {
+		static: {
+			directory: path.resolve(__dirname, './dist'),
+
+		},
+
+		watchFiles: [
+
+		],
+
+		compress: true,
+		historyApiFallback: true,
+		open: true,
+		// port: 8080
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js)$/,
+				use: 'babel-loader',
+			},
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					{ loader: 'css-loader', options: { importLoaders: 1 } },
+					'postcss-loader',
+				],
+			},
+			{
+				test: /\.(png|jpe?g|svg)$/i,
+				type: 'asset/resource',
+				generator: {
+					filename: 'images/[name][ext]',
+				},
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+		}),
+	],
+};
